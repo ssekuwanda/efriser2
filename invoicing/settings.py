623e37655d868fd795a24c33c2668766157e7309
ssekuwanda <douglasekuwanda@gmail.com>
2022-06-30 17:00:17 +0300

    added settings

--- invoicing/settings.py
+++ invoicing/settings.py
@@ -76,19 +76,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'invoicing.wsgi.application'
 
 
-if ONLINE:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL')
-        )
+# if ONLINE:
+#     DATABASES = {
+#         'default': dj_database_url.config(
+#             default=config('DATABASE_URL')
+#         )
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
