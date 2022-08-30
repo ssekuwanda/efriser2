23f9ab92fc63756eb77939086f9e4c261428b481
ssekuwanda <douglasekuwanda@gmail.com>
2022-08-30 13:47:11 +0300

    added setting

--- invoicing/settings.py
+++ invoicing/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 CSS_LOCATION = os.path.join(BASE_DIR,'static')
 
