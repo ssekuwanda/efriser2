8162c66d65f49858fd22d19d8f15f052ec06314e
ssekuwanda <cytixdoug@gmail.com>
2022-10-21 17:10:01 +0300

    edits

--- invoice/models.py
+++ invoice/models.py
@@ -262,8 +262,8 @@ class CnCancel(models.Model):
         ordering = ("-date_created",)
 
 class Unit_Measurement(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
-    code = models.CharField(max_length=3, null=True, blank=False)
+    name = models.CharField(max_length=400, null=False, blank=False)
+    code = models.CharField(max_length=400, null=True, blank=False)
 
     def __str__(self):
         return self.name
